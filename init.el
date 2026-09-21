d4a9e8dd3333ad8abaa7b1a566a88b56ece552a1
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-20 20:09:51 -0700

    Restored scroll bar

--- init.el
+++ init.el
@@ -3,7 +3,6 @@
 ;; ui configuration
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(scroll-bar-mode -1)
 (add-to-list 'default-frame-alist '(font . "Consolas-11"))
 (load-theme 'wombat)
 (setq-default cursor-type 'bar)
