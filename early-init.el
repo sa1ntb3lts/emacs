93cc4810130cc2fbce709898ca9874258e15e08b
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-21 00:40:49 -0700

    Center initial frame code

--- early-init.el
+++ early-init.el
@@ -4,6 +4,11 @@
 (setq gc-cons-threshold 63000000
       gc-cons-percentage 0.6)
 
+;; center initial frame
+(setq default-frame-alist
+      '((left . 540)
+        (top . 960)))
+
 ;; remove UI elements
 (push '(menu-bar-lines . 0) default-frame-alist)
 
