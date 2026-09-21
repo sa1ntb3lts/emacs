b87612c5dc981fd9e8d010e72398acb1aaf1f578
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-20 19:11:15 -0700

    Changed invalid character

--- init.el
+++ init.el
@@ -11,6 +11,6 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(unless (package-installed-p ‘use-package)
+(unless (package-installed-p 'use-package)
    (package-refresh-contents)
    (package-install ‘use-package))
