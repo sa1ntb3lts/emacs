2e7690ef388082294f1f359efcec4df4bdcb0ee5
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-20 18:21:44 -0700

    Updated comments

--- init.el
+++ init.el
@@ -1,24 +1,14 @@
-;; Start maximized
+;; Window
 (push '(fullscreen . maximized) default-frame-alist)
 
-;; Disable toolbar
+;; UI
 (tool-bar-mode -1)
-
-;; Disable menubar
 (menu-bar-mode -1)
-
-;; Disable scrollbar
 (scroll-bar-mode -1)
-
-;; Change font
 (add-to-list 'default-frame-alist '(font . "Consolas-11"))
-
-;; Change theme
 (load-theme 'wombat)
-
-;; Change cursor
 (setq-default cursor-type 'bar)
 
-;; Initialize packages
+;; Package
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
