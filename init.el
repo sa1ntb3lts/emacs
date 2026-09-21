a65a5c4da1f1819f9b23139ed43645dec0ed1032
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-20 21:21:26 -0700

    Removed toolbar

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 ;;; init.el --- Emacs configuration entry point -*- lexical-binding: t; -*-
 
-;; ui configuration
+;; UI configuration
+(tool-bar-mode -1)
+(menu-bar-mode -1)
 (add-to-list 'default-frame-alist '(font . "Consolas-11"))
 (load-theme 'wombat)
 (setq-default cursor-type 'bar)
