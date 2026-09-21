6bd7820e4676db5f7b06e9c41a20e90e73a39731
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-20 20:04:01 -0700

    Remove UI elements

--- early-init.el
+++ early-init.el
@@ -1,4 +1,9 @@
 ;;; early-init.el --- Emacs early configuration -*- lexical-binding: t; -*-
 
-;; Window
+;; start the initial frame maximized
 (push '(fullscreen . maximized) default-frame-alist)
+
+;; remove UI elements
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
