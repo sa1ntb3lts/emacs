c24ab1065efc03fe62de0c0670cdcdb520178f0f
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-20 18:56:19 -0700

    Changed code for packages

--- init.el
+++ init.el
@@ -6,9 +6,14 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (add-to-list 'default-frame-alist '(font . "Consolas-11"))
-(load-theme 'wombat)
+(load-theme 'wombat t)
 (setq-default cursor-type 'bar)
 
 ;; Package
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p ‘use-package)
+   (package-refresh-contents)
+   (package-install ‘use-package))
