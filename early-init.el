9344b408d88a2fe87c49c2ae85adc97ae73d1668
sa1ntb3lts <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-23 12:28:45 -0700

    Fixed center initial frame configuration

--- early-init.el
+++ early-init.el
@@ -4,10 +4,19 @@
 (setq gc-cons-threshold 63000000
       gc-cons-percentage 0.6)
 
-;; TODO: fix center initial frame
-(setq default-frame-alist
-      '((left . 540)
-        (top . 960)))
+;; center initial frame
+(defun frame-center ()
+  "Center the current frame."
+  (interactive)
+  (let* ((dw (display-pixel-width))
+         (dh (display-pixel-height))
+         (f  (selected-frame))
+         (fw (frame-pixel-width f))
+         (fh (frame-pixel-height f))
+         (x  (- (/ dw 2) (/ fw 2)))
+         (y  (- (/ dh 2) (/ fh 2))))
+    (message (format "dw %d dh %d fw %d fh %d x %d y %d" dw dh fw fh x y))
+    (set-frame-position f x y)))
 
 ;; remove UI elements
 (push '(menu-bar-lines . 0) default-frame-alist)
